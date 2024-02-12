1d66779f3f13c490be20e650e1597e428c4f8f03
project-srs <project@srs>
2024-02-12 17:42:33 +0900

    update

--- build_lecture/src/exec_sample2/main.cpp
+++ build_lecture/src/exec_sample2/main.cpp
@@ -1,5 +1,5 @@
 #include <build_lecture/exec_sample2/printer.hpp>
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   print_data();
   return 0;
--- build_lecture/src/exec_sample2/printer.cpp
+++ build_lecture/src/exec_sample2/printer.cpp
@@ -1,4 +1,5 @@
 #include <build_lecture/exec_sample2/printer.hpp>
-void print_data(void) {
+void print_data(void)
+{
   printf("execute print_data\n");
 }
