f968365ea66d4c0aabec86bb559c6570b6989620
project-srs <project@srs>
2024-02-12 22:03:57 +0900

    update

---
+++ rclcpp_param_lecture/launch/param_by_yaml.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rclcpp_param_lecture',
+            executable='param_server',
+            parameters=[PathJoinSubstitution([FindPackageShare('rclcpp_param_lecture'), 'config', 'param_server.yaml'])],
+            output='screen',
+            emulate_tty=True,
+        ),
+    ])
